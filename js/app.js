d55ee605d4d2368b295c5271e321c03964a0c3f0
RajBhavani <rbhavani@outlook.com>
2016-09-13 09:50:51 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,67 +1,65 @@
-$(document).ready(function() {
+$( document ).ready(function() {
+	'use strict';
 
 
+	var KEYS = { x: 88 };
+	var ryu = $( '.ryu' );
 
 
-	//get ryu into ready state on mouse-enter
-  $('.ryu').mouseenter(function() {
-  	$('.ryu-still').hide();
-  	$('.ryu-ready').show();
-  })
-  .mouseleave(function() {
-  	//get ryu back to still on mouse-leave
-    $('.ryu-ready').hide();
-	$('.ryu-still').show();
-  })
+	function playHadouken() {
+		// play sound
+		var hadouken = document.getElementById( 'hadouken-sound' );
+		hadouken.volume = 0.5;
+		hadouken.load();
+		hadouken.play();
+
+		// play animation
+		$( '.hadouken' )
+		.show()
+		.animate(
+			{ left: '300px' },
+			500,
+			function() {
+				$( this ).hide()
+				// reset css left
+				.removeAttr( 'style' );
+			}
+		)
+	}
 
 
-.mousedown(function() {
-  playHadouken();
-  $('.ryu-ready').hide();
-  $('.ryu-throwing').show();
-  $('.hadouken').finish().show()
-  .animate(
-    {'left': '1020px'},
-    500,
-    function() {
-      $(this).hide();
-      $(this).css('left', '520px');
-    }
-  );
-})
-  .mouseup(function() {
-  $('.ryu-throwing').hide();
-  $('.ryu-ready').show();
+	ryu
+	// ready position
+	.on( 'mouseenter mouseup', function() {
+		ryu
+		.addClass( 'ryu-ready' )
+		.removeClass( 'ryu-throwing ryu-still' );
+	})
+	// throw hadouken
+	.on( 'mousedown', function() {
+		ryu
+		.addClass( 'ryu-throwing' )
+		.removeClass( 'ryu-ready ryu-still' );
+		
+		playHadouken();
+	})
+	// restore sill pose
+	.on( 'mouseleave', function() {
+		ryu
+		.addClass( 'ryu-still' )
+		.removeClass( 'ryu-ready ryu-throwing' );
+	})
+	;
+
+
+	$( document )
+	.on( 'keydown', function( event ) {
+		if ( event.keyCode === KEYS.x ) {
+			ryu.addClass( 'ryu-cool' );
+		}
+	})
+	.on( 'keyup', function() {
+		ryu.removeClass( 'ryu-cool' );
+	});
+
 });
-  $('.hadouken').show().animate(
-  {'left': '1020px'},
-  500,
-  function() {
-    $(this).hide();
-    $(this).css('left', '520px');
-  }
-);
-
-/*NOT WORKING- ryu still gets hidden, but only on mouseleave, and cool ryu is not appearing at all. Look slike I need to put this block somewhere else, like maybe before mouseleave script? */
-$(document).keydown(function(e) {
-    if (e.keyCode == 88) {
-      
-      $('.ryu-still').hide();
-      $('.ryu-cool').show();
-    }   
-  })
-	.keyup(function(e) {
-    if (e.keyCode == 88) { 
-      $('.ryu-cool').hide();
-      $('.ryu-still').show();
-    }
-  });
-
-
-}); //document ready end
-
-function playHadouken () {
-  $('#hadouken-sound')[0].volume = 0.5;
-  $('#hadouken-sound')[0].load();
-  $('#hadouken-sound')[0].play();
-}
